6bcf4117870c0d78412c0bdaf55106ee965faa67
tmba <ut42.nu@gmail.com>
2025-05-31 20:18:56 +0900

    Canvasのスタイルを修正し、画面全体に表示されるように変更（Tailwindを強制適用）

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -6,7 +6,7 @@ import { Box } from "@/components/Box";
 
 export const Scene: React.FC = () => {
   return (
-    <Canvas className="w-screen h-screen fixed top-0 left-0">
+    <Canvas className="!w-screen !h-screen">
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} intensity={1} />
       <Box position={[-1.2, 0, 0]} />
