a222746296ea28efed8fbaaf5bc3cc96e3a94453
tmba <ut42.nu@gmail.com>
2025-05-31 20:03:21 +0900

    Three.jsのボックスコンポーネントを追加し、Canvasを使用してシーンを描画するように変更

--- src/app/three/page.tsx
+++ src/app/three/page.tsx
@@ -1,84 +1,47 @@
 "use client";
 
 import type { NextPage } from "next";
-import { useEffect } from "react";
+import React, { useRef, useState } from "react";
 import * as THREE from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
+type MeshProps = React.JSX.IntrinsicElements["mesh"];
 
 const Home: NextPage = () => {
-  let canvas: HTMLElement;
-  useEffect(() => {
-    if (canvas) return;
-    // canvasを取得
-    canvas = document.getElementById("canvas")!;
-
-    // シーン
-    const scene = new THREE.Scene();
-
-    // サイズ
-    const sizes = {
-      width: innerWidth,
-      height: innerHeight,
-    };
-
-    // カメラ
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      sizes.width / sizes.height,
-      0.1,
-      1000
+  const Box = (props: MeshProps) => {
+    const ref = useRef<THREE.Mesh>(null!);
+    const [hovered, setHover] = useState(false);
+    const [active, setActive] = useState(false);
+    useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+    return (
+      <mesh
+        {...props}
+        ref={ref}
+        scale={active ? 1.5 : 1}
+        onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}
+      >
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={hovered ? "hotpink" : "red"} />
+      </mesh>
     );
+  };
 
-    // レンダラー
-    const renderer = new THREE.WebGLRenderer({
-      canvas: canvas || undefined,
-      antialias: true,
-      alpha: true,
-    });
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(window.devicePixelRatio);
-
-    // ボックスジオメトリー
-    const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-    const boxMaterial = new THREE.MeshLambertMaterial({
-      color: "#2497f0",
-    });
-    const box = new THREE.Mesh(boxGeometry, boxMaterial);
-    box.position.z = -5;
-    box.rotation.set(10, 10, 10);
-    scene.add(box);
-
-    // ライト
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
-    scene.add(ambientLight);
-    const pointLight = new THREE.PointLight(0xffffff, 0.2);
-    pointLight.position.set(1, 2, 3);
-    scene.add(pointLight);
-
-    // アニメーション
-    const clock = new THREE.Clock();
-    const tick = () => {
-      const elapsedTime = clock.getElapsedTime();
-      box.rotation.x = elapsedTime;
-      box.rotation.y = elapsedTime;
-      window.requestAnimationFrame(tick);
-      renderer.render(scene, camera);
-    };
-    tick();
-
-    // ブラウザのリサイズ処理
-    window.addEventListener("resize", () => {
-      sizes.width = window.innerWidth;
-      sizes.height = window.innerHeight;
-      camera.aspect = sizes.width / sizes.height;
-      camera.updateProjectionMatrix();
-      renderer.setSize(sizes.width, sizes.height);
-      renderer.setPixelRatio(window.devicePixelRatio);
-    });
-  }, []);
   return (
-    <>
-      <canvas id="canvas"></canvas>
-    </>
+    <Canvas
+      style={{
+        width: 100 + "vw",
+        height: 100 + "vh",
+        position: "fixed",
+        top: 0,
+        left: 0,
+      }}
+    >
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
   );
 };
 
