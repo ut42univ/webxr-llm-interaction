cb232fa955af996b840139221da51fb524332e04
tmba <ut42.nu@gmail.com>
2025-05-31 21:01:59 +0900

    シーンのライティングを調整し、ambientLightの強度を0.5から0.3に変更、pointLightをdirectionalLightに置き換え

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -7,8 +7,8 @@ import { Box } from "@/components/Box";
 export const Scene: React.FC = () => {
   return (
     <Canvas className="!w-screen !h-screen">
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} intensity={1} />
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[10, 10, 5]} intensity={0.8} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} color="blue" hoverColor="lightblue" />
     </Canvas>
