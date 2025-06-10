93f08bc470bb576ea105d23692f87fa5020b955c
tmba <ut42.nu@gmail.com>
2025-06-10 16:02:07 +0900

    シーンコンポーネントの照明と環境設定を更新し、オービットコントロールを追加

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -4,17 +4,30 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Box } from "@/components/Box";
 import { createXRStore, XR } from "@react-three/xr";
+import { Environment, OrbitControls } from "@react-three/drei";
 
 export const store = createXRStore();
 
 export const Scene: React.FC = () => {
   return (
-    <Canvas className="!w-screen !h-screen">
+    <Canvas
+      className="!w-screen !h-screen"
+      camera={{ position: [0, 0, 12], fov: 30 }}
+    >
       <XR store={store}>
-        <ambientLight intensity={0.3} />
-        <directionalLight position={[10, 10, 5]} intensity={0.8} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} color="blue" hoverColor="lightblue" />
+        <hemisphereLight intensity={0.5} color="white" groundColor="black" />
+        <Environment
+          files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/8k/roof_garden_8k.hdr"
+          ground={{ height: 5, radius: 40, scale: 20 }}
+        />
+        <Box position={[-1.2, 1.5, 0]} />
+        <Box position={[1.2, 1.5, 0]} color="blue" hoverColor="lightblue" />
+        <OrbitControls
+          autoRotateSpeed={0.85}
+          zoomSpeed={0.75}
+          minPolarAngle={Math.PI / 2.5}
+          maxPolarAngle={Math.PI / 2.55}
+        />
       </XR>
     </Canvas>
   );
