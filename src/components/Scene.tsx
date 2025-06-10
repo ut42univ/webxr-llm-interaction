375132296a0936daf8e5663a73507413ea6e2408
tmba <ut42.nu@gmail.com>
2025-06-10 22:24:47 +0900

    シーンコンポーネントに影を追加し、ボックスと球体に影を受ける機能を実装

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -13,11 +13,19 @@ export const store = createXRStore();
 export const Scene: React.FC = () => {
   return (
     <Canvas
+      shadows
       className="!w-screen !h-screen"
       camera={{ position: [0, 0, 12], fov: 30 }}
     >
       <XR store={store}>
         <hemisphereLight intensity={0.5} color="white" groundColor="black" />
+        <directionalLight
+          castShadow
+          position={[5, 10, 5]}
+          intensity={1.0}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+        />
         <Suspense fallback={<EnvironmentFallback />}>
           <Environment
             files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/roof_garden_4k.hdr"
@@ -25,10 +33,30 @@ export const Scene: React.FC = () => {
           />
         </Suspense>
 
-        <Box position={[-4.0, 1.5, 0]} />
-        <Box position={[4.0, 1.5, 0]} color="blue" hoverColor="lightblue" />
+        <Box position={[-4.0, 1.5, 0]} castShadow />
+        <Box
+          position={[4.0, 1.5, 0]}
+          color="blue"
+          hoverColor="lightblue"
+          castShadow
+        />
 
-        <Sphere position={[0, 1.0, 0]} color="orange" hoverColor="yellow" />
+        <Sphere
+          position={[0, 1.0, 0]}
+          color="orange"
+          hoverColor="yellow"
+          castShadow
+        />
+
+        {/* 影を受けるための地面 */}
+        <mesh
+          receiveShadow
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, 0, 0]}
+        >
+          <planeGeometry args={[100, 100]} />
+          <shadowMaterial opacity={0.6} />
+        </mesh>
 
         <OrbitControls
           autoRotateSpeed={0.85}
