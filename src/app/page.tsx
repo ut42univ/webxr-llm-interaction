f1532ad0a78521970fb8364d0285a71c390db54f
tmba <ut42.nu@gmail.com>
2025-06-10 11:57:00 +0900

    installing shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,17 @@
 import type { NextPage } from "next";
 import React from "react";
 import { Scene } from "@/components/Scene";
+import { createXRStore } from "@react-three/xr";
+
+const store = createXRStore();
 
 const HomePage: NextPage = () => {
-  return <Scene />;
+  return (
+    <>
+      <button onClick={() => store.enterAR()}>Enter AR</button>
+      <Scene />
+    </>
+  );
 };
 
 export default HomePage;
