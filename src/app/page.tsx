c243c253ba45d5b62a05ea8712cc6f6a0e2a2421
tmba <ut42.nu@gmail.com>
2025-06-15 15:36:44 +0900

    シーンコンポーネントに没入モードボタンを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,20 @@
 
 import type { NextPage } from "next";
 import React from "react";
-import { Scene } from "@/components/Scene";
+import { Scene, store } from "@/components/Scene";
+import { Button } from "@/components/ui/button";
+import { RectangleGoggles } from "lucide-react";
 
 const HomePage: NextPage = () => {
   return (
     <div>
+      <Button
+        className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-10"
+        onClick={() => store.enterVR()}
+      >
+        <RectangleGoggles />
+        没入モードに移行
+      </Button>
       <Scene />
     </div>
   );
