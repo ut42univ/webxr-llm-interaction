7da0eb343b6ad191da843ad8cd9d0b071d7d67cf
tmba <ut42.nu@gmail.com>
2025-06-15 17:56:01 +0900

    CameraコンポーネントのOrbitControlsのプロパティを削除し、デフォルト設定に変更

--- src/components/xr/common/Camera.tsx
+++ src/components/xr/common/Camera.tsx
@@ -7,12 +7,7 @@ export const Camera: React.FC = () => {
   return (
     <>
       <PerspectiveCamera makeDefault position={[0, 0, 12]} fov={30} />
-      <OrbitControls
-        autoRotateSpeed={0.85}
-        zoomSpeed={0.75}
-        minPolarAngle={Math.PI / 2.5}
-        maxPolarAngle={Math.PI / 2.55}
-      />
+      <OrbitControls />
     </>
   );
 };
